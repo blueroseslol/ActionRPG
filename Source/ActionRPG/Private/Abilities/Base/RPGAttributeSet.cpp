8331a25565ef93a75a75b4e3d361d69c3d5462c7
bluerose <378100977@qq.com>
2019-07-31 16:06:39 +0800

    1、添加RPGAttributeSet的代码

--- Source/ActionRPG/Private/Abilities/Base/RPGAttributeSet.cpp
+++ Source/ActionRPG/Private/Abilities/Base/RPGAttributeSet.cpp
@@ -1,5 +1,45 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+#include "Abilities/Base/RPGAttributeSet.h"
+#include "Abilities/Base/RPGAbilitySystemComponent.h"
+//#include "RPGCharacterBase.h"
+//#include "GameplayEffect.h"
+//#include "GameplayEffectExtension.h"
 
+URPGAttributeSet::URPGAttributeSet() :
+	Health(100), MaxHealth(100),
+	AttackPower(1), DefensePower(0), Damage(0), MoveSpeed(300)
+{
+}
 
-#include "RPGAttributeSet.h"
+void URPGAttributeSet::PreAttributeChange(const FGameplayAttribute & Attribute, float & NewValue)
+{
+	Super::PreAttributeChange(Attribute, NewValue);
 
+	if (Attribute == GetMaxHealthAttribute())
+	{
+		AdjustAttributeForMaxChange(Health, MaxHealth, NewValue, GetHealthAttribute());
+	}
+	else if (Attribute == GetMaxManaAttribute())
+	{
+		AdjustAttributeForMaxChange(Mana, MaxMana, NewValue, GetManaAttribute());
+	}
+}
+
+void URPGAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData & Data)
+{
+	Super::PostGameplayEffectExecute(Data);
+
+}
+
+void URPGAttributeSet::AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty)
+{
+	UAbilitySystemComponent* AbilityComp = GetOwningAbilitySystemComponent();
+	const float CurrentMaxValue = MaxAttribute.GetCurrentValue();
+	if (!FMath::IsNearlyEqual(CurrentMaxValue, NewMaxValue) && AbilityComp)
+	{
+		// Change current value to maintain the current Val / Max percent
+		const float CurrentValue = AffectedAttribute.GetCurrentValue();
+		float NewDelta = (CurrentMaxValue > 0.f) ? (CurrentValue * NewMaxValue / CurrentMaxValue) - CurrentValue : NewMaxValue;
+
+		AbilityComp->ApplyModToAttributeUnsafe(AffectedAttributeProperty, EGameplayModOp::Additive, NewDelta);
+	}
+}
--- Source/ActionRPG/Public/Abilities/Base/RPGAttributeSet.h
+++ Source/ActionRPG/Public/Abilities/Base/RPGAttributeSet.h
@@ -4,8 +4,15 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "RPGAttributeSet.generated.h"
 
+//使用AttributeSet.h的宏
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 /**
  * 
  */
@@ -13,5 +20,52 @@ UCLASS()
 class ACTIONRPG_API URPGAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+public:
+	URPGAttributeSet();
+	//重写接口
+	virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+	virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+	//生命值
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, Health)
+
+	//最大生命值
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, MaxHealth)
+
+	//生命值
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, Mana)
+
+	//最大生命值
+	UPROPERTY(BlueprintReadOnly, Category = "Mana")
+	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, MaxMana)
+
+	//攻击力
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, AttackPower)
+
+	//防御力
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, DefensePower)
+
+	//魔法伤害
+	UPROPERTY(BlueprintReadOnly, Category = "Mana", meta = (HideFromLevelInfos))
+	FGameplayAttributeData Damage;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, Damage)
+
+	//移动速度
+	UPROPERTY(BlueprintReadOnly, Category = "MoveSpeed")
+	FGameplayAttributeData MoveSpeed;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, MoveSpeed)
+public:
+	//当最大值改变时，将当前值数修改为正确的值
+	void AdjustAttributeForMaxChange(FGameplayAttributeData& AffectedAttribute, const FGameplayAttributeData& MaxAttribute, float NewMaxValue, const FGameplayAttribute& AffectedAttributeProperty);
 };
