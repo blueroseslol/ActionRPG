6120790429b4665459e261b4deec80d2677175fa
bluerose <378100977@qq.com>
2019-08-01 17:35:30 +0800

    1、创建了RPGCharacter类以及蓝图子类，并且替换关卡中的角色 2、创建若干空类

---
+++ Source/ActionRPG/Public/Character/RPGCharacterAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Base/RPGAttributeSet.h"
+#include "RPGCharacterAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPG_API URPGCharacterAttributeSet : public URPGAttributeSet
+{
+	GENERATED_BODY()
+	
+};
