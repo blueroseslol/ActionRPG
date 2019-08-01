99ed2c09f3f12e49cd976296d518e9cd34b0a80b
bluerose <378100977@qq.com>
2019-08-01 17:33:24 +0800

    1、添加了ActionRPG案例中的URPGGameplayAbility类与其所需的URPGTargetType、FRPGGameplayEffectContainer、FRPGGameplayEffectContainerSpec

--- Source/ActionRPG/Public/Abilities/Base/RPGGameplayAbility.h
+++ Source/ActionRPG/Public/Abilities/Base/RPGGameplayAbility.h
@@ -1,8 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayAbility.h"
+#include "GameplayTagContainer.h"
+#include "Abilities/Base/RPGAbilityTypes.h"
 #include "Abilities/GameplayAbility.h"
 #include "RPGGameplayAbility.generated.h"
 
@@ -13,5 +14,26 @@ UCLASS()
 class ACTIONRPG_API URPGGameplayAbility : public UGameplayAbility
 {
 	GENERATED_BODY()
-	
+public:
+	URPGGameplayAbility();
+
+	/** Gameplay标签与GameplayEffect Map */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = GameplayEffects)
+	TMap<FGameplayTag, FRPGGameplayEffectContainer> EffectContainerMap;
+
+	/** 读取指定的FRPGGameplayEffectContainer来生成FRPGGameplayEffectContainerSpec */
+	UFUNCTION(BlueprintCallable, Category = Ability, meta = (AutoCreateRefTerm = "EventData"))
+	virtual FRPGGameplayEffectContainerSpec MakeEffectContainerSpecFromContainer(const FRPGGameplayEffectContainer& Container, const FGameplayEventData& EventData, int32 OverrideGameplayLevel = -1);
+
+	/** 通过GameplayTag来搜索EffectContainerMap，并且生成FRPGGameplayEffectContainerSpec */
+	UFUNCTION(BlueprintCallable, Category = Ability, meta = (AutoCreateRefTerm = "EventData"))
+	virtual FRPGGameplayEffectContainerSpec MakeEffectContainerSpec(FGameplayTag ContainerTag, const FGameplayEventData& EventData, int32 OverrideGameplayLevel = -1);
+
+	/** 让FRPGGameplayEffectContainerSpec中的effect对指定目标生效 */
+	UFUNCTION(BlueprintCallable, Category = Ability)
+	virtual TArray<FActiveGameplayEffectHandle> ApplyEffectContainerSpec(const FRPGGameplayEffectContainerSpec& ContainerSpec);
+
+	/** 调用MakeEffectContainerSpec生成FRPGGameplayEffectContainerSpec，再让Effect对目标生效 */
+	UFUNCTION(BlueprintCallable, Category = Ability, meta = (AutoCreateRefTerm = "EventData"))
+	virtual TArray<FActiveGameplayEffectHandle> ApplyEffectContainer(FGameplayTag ContainerTag, const FGameplayEventData& EventData, int32 OverrideGameplayLevel = -1);
 };
